6ea8fb6246111f10ecd4078178b08a059909a49e
Jaehyun Lim <35042807+lim-mlcs@users.noreply.github.com>
2018-06-23 22:26:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='RL_mlcs',
       version='0.0.2',
       install_requires=['gym>=0.2.3'],
       description='reinforcement learning agents using Gazebo and ROS.',
-      url='https://github.com/erlerobot/gym',
+      url='https://github.com/MLCS-Yonsei/Factory_RL_Gazebo',
       author='Seungchul ha',
       package_data={'RL_mlcs': ['launch/*.launch', 'worlds/*']},
 )
