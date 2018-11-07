3fb70b4a7cf13caccc10d117a07fce8afb8a7f26
lim <lim.yonsei@gmail.com>
2018-11-07 18:14:38 +0900

    minor update

--- module/replay.py
+++ module/replay.py
@@ -13,7 +13,6 @@ class Replay(object):
                         ['action','reward','done']
         self.buffer = {item:[] for item in self.bufferkeys}
         self.buffersize = 0
-        self.max = False
 
     def batch(self):
         if self.buffersize > self.batch_size:
@@ -22,29 +21,41 @@ class Replay(object):
                 self.batch_size,
                 replace=False
             )
-            Batch = {key:[] for key in self.buffer.keys()}
-            for name in self.buffer.keys():
+            Batch = {key:[] for key in self.bufferkeys}
+            for name in self.bufferkeys:
                 for idx in indices:
                     Batch[name].append(self.buffer[name][idx])
             return Batch
         else:
             return self.buffer
 
-    def add(self,experience):
-        if (self.currentPosition >= self.max_buffer-1):
-            self.currentPosition = 0
-            self.max = True
-        if self.max:
-            for item in self.buffer.keys():
+    def add(self, experience):
+        self.currentPosition += 1
+        if self.buffersize >= self.max_buffer:
+            if self.currentPosition > self.max_buffer-1:
+                self.currentPosition = 0
+            for item in self.bufferkeys:
                 self.buffer[item][self.currentPosition] = experience[item]
         else:
-            for item in self.buffer.keys():
+            for item in self.bufferkeys:
                 self.buffer[item].append(experience[item])
             self.buffersize += 1
-        self.currentPosition += 1
+        self.buffer['buffersize'] = self.buffersize
+        self.buffer['currentPosition'] = self.currentPosition
     
+    def load(self, buffer):
+        self.buffersize = max(buffer['buffersize'], self.max_buffer)
+        idx = buffer['currentPosition']+1
+        if self.currentPosition > self.max_buffer-1:
+            self.currentPosition = self.max_buffer-1
+            for key in self.bufferkeys:
+                self.buffer[key] = buffer[key][idx-self.max_buffer:idx]
+        else:
+            self.currentPosition = buffer['currentPosition']
+            for key in self.bufferkeys:
+                self.buffer[key] = buffer[key][0:idx]+buffer[key][-self.max_buffer+idx:]
+
     def clear(self):
         self.currentPosition = -1
         self.buffersize = 0
         self.buffer = {item:[] for item in self.bufferkeys}
-        self.max = False
